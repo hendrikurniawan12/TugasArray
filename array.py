fe8f83986c88ef8e7139c1106e9e07cc27c3ab5b
hendrikurniawan12 <122343986+hendrikurniawan12@users.noreply.github.com>
2023-01-16 08:54:44 +0700

    Add files via upload

---
+++ array.py
@@ -0,0 +1,6 @@
+from array import*
+# buat variabrl array i yang, masuk
+nilai = array('b',[1,2,3,4,5])
+#menampilkan array
+for n in nilai:
+	print(n)
---
+++ arraydaudimensi.py
@@ -0,0 +1,13 @@
+#total nilai nilai latihan
+
+#siswa 1 - 60 55 75 82
+#siswa 2 - 75 85 75
+#siswa 3 - 66 85 75 79
+#siswa 4 - 88 33 89 86
+
+nilai =[[60,55,75,82],[75,96,75],[66,85,73,79],[88,33,89,86]]
+
+print(nilai[0])
+print(nilai[2])
+print(nilai[0][2])
+print(nilai[2][0])
